da46d6ad073f1d0ef40dd6356db2cb204ce475f5
Jonathan Feinberg <jdf@pobox.com>
2009-01-09 21:09:26 +0000

    Added shift to constrain camera

--- src/peasy/PeasyCam.java
+++ src/peasy/PeasyCam.java
@@ -18,6 +18,7 @@
  */
 package peasy;
 
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
 import peasy.org.apache.commons.math.geometry.Rotation;
@@ -33,16 +34,23 @@ public class PeasyCam
 	private static final Vector3D LOOK = Vector3D.minusK;
 	private static final Vector3D UP = Vector3D.minusJ;
 
-	final PApplet p;
+	private static enum Constraint
+	{
+		X, Y
+	}
 
-	final double startDistance;
-	final Vector3D startCenter;
+	private final PApplet p;
 
-	PeasyMouseListener mouseListener = null;
+	private final double startDistance;
+	private final Vector3D startCenter;
 
-	double distance;
-	Vector3D center;
-	Rotation rotation;
+	private PeasyMouseListener mouseListener = null;
+
+	private double distance;
+	private Vector3D center;
+	private Rotation rotation;
+
+	private Constraint dragConstraint = null;
 
 	public final String VERSION = "0.1.0";
 
@@ -71,7 +79,9 @@ public class PeasyCam
 				PApplet.println("PeasyCam is already listening to mouse.");
 				return;
 			}
-			p.registerMouseEvent(mouseListener = new PeasyMouseListener());
+			mouseListener = new PeasyMouseListener();
+			p.registerMouseEvent(mouseListener);
+			p.registerKeyEvent(mouseListener);
 		}
 		else
 		{
@@ -81,6 +91,7 @@ public class PeasyCam
 				return;
 			}
 			p.unregisterMouseEvent(mouseListener);
+			p.unregisterKeyEvent(mouseListener);
 			mouseListener = null;
 		}
 	}
@@ -92,17 +103,40 @@ public class PeasyCam
 
 	protected class PeasyMouseListener
 	{
+		public void keyEvent(final KeyEvent e)
+		{
+			if (e.getID() == KeyEvent.KEY_RELEASED && e.getKeyCode() == KeyEvent.VK_SHIFT)
+				dragConstraint = null;
+		}
+
 		public void mouseEvent(final MouseEvent e)
 		{
 			if (e.getID() == MouseEvent.MOUSE_CLICKED && e.getClickCount() == 2)
 			{
 				reset();
-
+			}
+			else if (e.getID() == MouseEvent.MOUSE_RELEASED)
+			{
+				dragConstraint = null;
 			}
 			else if (e.getID() == MouseEvent.MOUSE_DRAGGED)
 			{
 				float dx = p.mouseX - p.pmouseX;
 				float dy = p.mouseY - p.pmouseY;
+
+				if (e.isShiftDown())
+				{
+					if (dragConstraint == null && Math.abs(dx - dy) > 1)
+					{
+						dragConstraint = Math.abs(dx) > Math.abs(dy) ? Constraint.X
+								: Constraint.Y;
+					}
+				}
+				else
+				{
+					dragConstraint = null;
+				}
+
 				final int b = p.mouseButton;
 				if (b == PConstants.CENTER || (b == PConstants.LEFT && e.isMetaDown()))
 					mousePan(dx, dy);
@@ -129,7 +163,8 @@ public class PeasyCam
 		private void mousePan(final float dxMouse, final float dyMouse)
 		{
 			final double panScale = Math.sqrt(distance * .005);
-			pan(-dxMouse * panScale, dyMouse * panScale);
+			pan(dragConstraint == Constraint.Y ? 0 : -dxMouse * panScale,
+					dragConstraint == Constraint.X ? 0 : dyMouse * panScale);
 		}
 
 		private void mouseRotate(final float dx, final float dy)
@@ -137,12 +172,18 @@ public class PeasyCam
 			final Vector3D u = LOOK.scalarMultiply(distance).negate();
 			final double rotationScale = Math.sqrt(distance * .05);
 
-			final Vector3D vx = u.add(new Vector3D(dx * rotationScale, 0, 0));
-			rotateY(Vector3D.angle(u, vx) * (dx < 0 ? -1 : 1));
+			if (dragConstraint != Constraint.Y)
+			{
+				final Vector3D vx = u.add(new Vector3D(dx * rotationScale, 0, 0));
+				rotateY(Vector3D.angle(u, vx) * (dx < 0 ? -1 : 1));
+			}
 
-			final Vector3D vy = u.add(new Vector3D(0, dy * rotationScale, 0));
-			final double yAngle = Vector3D.angle(u, vy) * (dy < 0 ? -1 : 1);
-			rotateX(yAngle);
+			if (dragConstraint != Constraint.X)
+			{
+				final Vector3D vy = u.add(new Vector3D(0, dy * rotationScale, 0));
+				final double yAngle = Vector3D.angle(u, vy) * (dy < 0 ? -1 : 1);
+				rotateX(yAngle);
+			}
 		}
 	}
 
@@ -191,4 +232,5 @@ public class PeasyCam
 		rotation = rotation.applyTo(new Rotation(Vector3D.plusK, angle));
 		feed();
 	}
+
 }
