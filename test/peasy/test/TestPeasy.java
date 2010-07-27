403fb485380015e26676f39e90dc08fe2902f283
Jonathan Feinberg <jdf@pobox.com>
2010-07-26 22:02:58 -0400

    Add driver for testing without building

---
+++ test/peasy/test/TestPeasy.java
@@ -0,0 +1,30 @@
+package peasy.test;
+
+import peasy.PeasyCam;
+import processing.core.PApplet;
+
+@SuppressWarnings("serial")
+public class TestPeasy extends PApplet {
+
+	PeasyCam cam;
+
+	public void setup() {
+		size(200, 200, P3D);
+		cam = new PeasyCam(this, 100);
+		cam.setMinimumDistance(50);
+		cam.setMaximumDistance(500);
+	}
+
+	public void draw() {
+		rotateX(-.5f);
+		rotateY(-.5f);
+		background(0);
+		fill(255, 0, 0);
+		box(30);
+		pushMatrix();
+		translate(0, 0, 20);
+		fill(0, 0, 255);
+		box(5);
+		popMatrix();
+	}
+}
